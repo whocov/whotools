4315f053534f7e3a33231f7ff23043f7790a6c48
Henry Laurenson-Schafer <henry.laurenson.schafer@gmail.com>
2023-07-14 11:59:22 +0200

    adding imports

--- R/whotools-package.R
+++ R/whotools-package.R
@@ -4,9 +4,12 @@
 #' @import purrr
 
 ## usethis namespace: start
+#' @importFrom dplyr case_when
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
-#' @importFrom dplyr case_when
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom slider slide_index_dbl
 #' @importFrom stringr str_glue
+#' @importFrom stringr str_replace
 ## usethis namespace: end
 NULL
